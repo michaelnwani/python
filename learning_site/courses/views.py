a669b76ff7f03b38798ab8193d851e5c57a63a61
Michael <kmichael24@gmail.com>
2015-09-12 12:23:10 -0400

    typo

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Course
 
 
+
 def course_list(request):
     courses = Course.objects.all()
     return render(request, 'courses/course_list.html', {'courses' : courses})
@@ -11,4 +12,4 @@ def course_list(request):
 def course_detail(request, pk):
     # course = Course.objects.get(pk=pk)
     course = get_object_or_404(Course, pk=pk)
-    return render(request, 'courses/course_detail.html', {'course' : course})
+    return render(request, 'course      s/course_detail.html', {'course' : course})
