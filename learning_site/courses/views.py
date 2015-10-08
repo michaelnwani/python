8e344ed0891372c547f44cc4d3beb6c3de86bcbc
Michael <kmichael24@gmail.com>
2015-10-08 18:02:22 -0400

    updated django app

--- learning_site/courses/views.py
+++ learning_site/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import Course
+from .models import Course, Step
 
 
 
@@ -14,5 +14,9 @@ def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course' : course})
     
+
+def step_detail(request, course_pk, step_pk):
+    step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
+    return render(request, 'courses/step_detail.html', {'step' : step})
     
     
